42777f98801647535804fd2fd522a047ce89bbe2
Poden <56049210+Poden@users.noreply.github.com>
2020-03-29 21:27:13 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,51 @@ import React from 'react';
 
 function App() {
   return (
-    <main className='App'>
-      {/* content goes here */}
+  
+    <header>
+      <h1>   Sally Student's Resume   </h1>
+   </header>
+      <main>
+      <section>
+         <h3> Sallys education </h3>
+         <ul>
+         <li> Sally graduated from Texas Tech university with an undergraduate degree in Chemisrty.
+         </li>
+         </ul>
+      </section>
+      <section>
+         <h3>Sally's Employment</h3>
+         <h4>Matrix Inc.</h4> 
+         <ul>
+         <li>Lab Technician</li>
+         <li>Employed from April 2016 - January 2019</li>
+         <li>Analytical testing of oils, Data Collection, Reviewing data to present to scientist
+            , Working closely with scientist
+         </li>
+         </ul>
+         <h4>Huntsman</h4>
+         <ul>
+         <li>Senior Lab Technician</li>
+         <li>Employed from January 2019 - Current</li>
+         <li>Thermal analysis, sample preparation, chemical and physical tests, Properly collect, record, evaluate 
+            and report date in a technical writing format Literature searching
+            Write reports and presentations
+            Participate with team in safety and housekeeping activities
+            Other General Responsibilities, Follows instructions and performs other duties as may be assigned by supervisor,
+            Work collaboratively with team members in accomplishment of Huntsman company goals. 
+         </li>
+         </ul>
+      </section>
+      <section>
+         <h3>Contact Information</h3>
+         <address>
+               <a href="mailto:Sally.Student@ttu.edusubject=Sally'sResume" target="blank">Sally.Student@ttu.edu</a>
+               <div>214-815-0908</div>
+        </address>
+         <ul>
+            <li>Sally Student</li>
+         </ul>
+      </section>
     </main>
   );
 }
